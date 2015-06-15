e6274078ec8e005a49df3abf5bbce11bdd2e8d52
Christopher Armenio <christopher.armenio@opencxa.org>
2015-06-14 20:39:38 -0400

    added numberUtils header

---
+++ include/misc/cxa_numberUtils.h
@@ -0,0 +1,43 @@
+/**
+ * Copyright 2013 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef CXA_NUMBERUTILS_H_
+#define CXA_NUMBERUTILS_H_
+
+
+/**
+ * @file
+ * This is a file which contains utility functions for manipulating numbers
+ *
+ * @author Christopher Armenio
+ */
+
+
+// ******** includes ********
+#include <stdbool.h>
+
+
+// ******** global macro definitions ********
+#define CXA_MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define CXA_MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+
+// ******** global type definitions *********
+
+
+// ******** global function prototypes ********
+
+
+#endif // CXA_NUMBERUTILS_H_
