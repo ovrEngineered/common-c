5ed45ec8f5db2713f106057248bb8dc541c037be
Christopher Armenio <christopher.armenio@opencxa.org>
2015-10-02 19:01:57 -0400

    added time base module for ble112 architecture

---
+++ include/arch-ble112/cxa_ble112_timeBase.h
@@ -0,0 +1,65 @@
+/**
+ * @file
+ * This file contains prototypes and an architecture-specific implementation of a
+ * timeBase object for the BLE112 C SDK. A timeBase object is essentially a
+ * monotonically increasing counter which can be used to calculate macro-scale,
+ * relative timeouts (on the order of seconds and minutes).
+ *
+ * @note This file contains functionality in addition to that already provided in @ref cxa_timeBase.h
+ *
+ *
+ *
+ * #### Example Usage: ####
+ *
+ * @code
+ * @endcode
+ *
+ *
+ * @copyright 2013-2015 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#ifndef CXA_BLE112_TIMEBASE_H_
+#define CXA_BLE112_TIMEBASE_H_
+
+
+// ******** includes ********
+#include <stdint.h>
+#include <cxa_timeBase.h>
+
+
+// ******** global macro definitions ********
+
+
+// ******** global type definitions *********
+/**
+ * @private
+ */
+struct cxa_timeBase
+{
+	void* _placeHolder;
+};
+
+
+// ******** global function prototypes ********
+/**
+ * @public
+ * @brief Initializes a timeBase object to use the system's
+ * 24-bit sleep timer
+ *
+ * @param[in] tbIn pointer to a pre-allocated BLE112 timeBase object
+ */
+void cxa_ble112_timeBase_init(cxa_timeBase_t *const tbIn);
+
+
+#endif // CXA_BLE112_TIMEBASE_H_
