277c7a883d49fb3a5f1e7b46d1c4b1f2a3c83dcd
Christopher Armenio <christopher.armenio@opencxa.org>
2015-10-25 17:51:20 -0400

    updated header

--- src/logger/cxa_logger.c
+++ src/logger/cxa_logger.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013 opencxa.org
+ * Copyright 2015 opencxa.org
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,13 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-#include "cxa_logger_implementation.h"
-
-
-/**
+ *
  * @author Christopher Armenio
  */
+#include "cxa_logger_implementation.h"
 
 
 // ******** includes ********
