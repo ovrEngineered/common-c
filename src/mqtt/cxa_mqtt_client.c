227d226e5a24d1cb0aa27b9eacfc17e075abba05
Christopher Armenio <christopher.armenio@opencxa.org>
2015-11-08 23:54:42 -0500

    paho mqtt WIP

---
+++ src/mqtt/cxa_mqtt_client.c
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2015 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#include "cxa_esp8266_mqttClient.h"
+
+
+// ******** includes ********
+#include <string.h>
+#include <stddef.h>
+#include <user_interface.h>
+#include <cxa_assert.h>
+
+#define CXA_LOG_LEVEL			CXA_LOG_LEVEL_TRACE
+#include <cxa_logger_implementation.h>
+
+
+// ******** local macro definitions ********
+
+
+// ******** local type definitions ********
+
+
+// ******** local function prototypes ********
+
+
+// ********  local variable declarations *********
+
+
+// ******** global function implementations ********
+
+
+// ******** local function implementations ********
