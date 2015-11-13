43bcae6a828ed8ff7527b9e0b2443e623b80ad60
Christopher Armenio <christopher.armenio@opencxa.org>
2015-11-12 19:30:04 -0500

    added methods to fifo for "listening" to when a fifo becomes non-full

--- include/collections/cxa_fixedFifo.h
+++ include/collections/cxa_fixedFifo.h
@@ -26,20 +26,40 @@
 // ******** includes ********
 #include <stdlib.h>
 #include <stdbool.h>
+#include <cxa_array.h>
 
 
 // ******** global macro definitions ********
+#ifndef CXA_FF_MAX_LISTENERS
+	#define CXA_FF_MAX_LISTENERS		1
+#endif
+
 #define cxa_fixedFifo_initStd(fifoIn, onFullActionIn, bufferIn)						cxa_fixedFifo_init((fifoIn), (onFullActionIn), sizeof(*(bufferIn)), ((void*)(bufferIn)), sizeof(bufferIn))
 
 
 // ******** global type definitions *********
+typedef struct cxa_fixedFifo cxa_fixedFifo_t;
+
+
 typedef enum
 {
 	CXA_FF_ON_FULL_DEQUEUE,
 	CXA_FF_ON_FULL_DROP
 }cxa_fixedFifo_onFullAction_t;
 
-typedef struct  
+
+typedef void (*cxa_fixedFifo_cb_noLongerFull_t)(cxa_fixedFifo_t *const fifoIn, void* userVarIn);
+
+
+typedef struct
+{
+	cxa_fixedFifo_cb_noLongerFull_t cb_noLongerFull;
+
+	void* userVarIn;
+}cxa_fixedFifo_listener_entry_t;
+
+
+struct cxa_fixedFifo
 {
 	void *bufferLoc;
 	
@@ -50,11 +70,16 @@ typedef struct
 	size_t maxNumElements;
 	
 	cxa_fixedFifo_onFullAction_t onFullAction;
-}cxa_fixedFifo_t;
+
+	cxa_array_t listeners;
+	cxa_fixedFifo_listener_entry_t listeners_raw[CXA_FF_MAX_LISTENERS];
+};
 
 
 // ******** global function prototypes ********
 void cxa_fixedFifo_init(cxa_fixedFifo_t *const fifoIn, cxa_fixedFifo_onFullAction_t onFullActionIn, const size_t datatypeSize_bytesIn, void *const bufferLocIn, const size_t bufferMaxSize_bytesIn);
+
+void cxa_fixedFifo_addListener(cxa_fixedFifo_t *const fifoIn, cxa_fixedFifo_cb_noLongerFull_t cb_noLongerFull, void* userVarIn);
 
 bool cxa_fixedFifo_queue(cxa_fixedFifo_t *const fifoIn, void *const elemIn);
 
