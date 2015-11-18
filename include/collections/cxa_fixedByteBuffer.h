4ac687c8546b2a269521386d282b46b9da08687c
Christopher Armenio <christopher.armenio@opencxa.org>
2015-11-17 22:28:11 -0500

    added some comments and function to append/get prefixed strings

--- include/collections/cxa_fixedByteBuffer.h
+++ include/collections/cxa_fixedByteBuffer.h
@@ -1,5 +1,9 @@
 /**
  * @file
+ * This file contains an implementation of a statically allocated, fixed-max-length buffer/
+ * array that is specifically targetted at byte-level manipulation. If you need to store
+ * binary data and access it as bytes, 16-bit integers, 32-bit integers, this is the
+ * appropriate class.
  *
  * @note This object should work across all architecture-specific implementations
  *
@@ -7,23 +11,29 @@
  * #### Example Usage: ####
  *
  * @code
- * cxa_fixedByteBuffer_t myBuffer;
- * uint8_t myBuffer_buffer[16];			// where the data is actually stored
+ * cxa_fixedByteBuffer_t myFbb;
+ * uint8_t myFbb_buffer[16];			// where the data is actually stored
  *
  * // initialize the buffer with a maximum of 16 elements
- * cxa_fixedByteBuffer_init(&myBuffer, (void*)myBuffer_buffer, sizeof(myBuffer_buffer));
+ * cxa_fixedByteBuffer_init(&myFbb, (void*)myBuffer_buffer, sizeof(myBuffer_buffer));
+ * // OR more simply:
+ * cxa_fixedByteBuffer_initStd(&myFbb, myBuffer_buffer);
  *
  * ...
  *
  * // add two new values to the buffer (0x47 and 0x65)
- * cxa_fixedByteBuffer_append_byte(&myBuffer, 0x47);
- * cxa_fixedByteBuffer_append_byte(&myBuffer, 0x65);
+ * cxa_fixedByteBuffer_append_uint8(&myFbb, 0x47);
+ * cxa_fixedByteBuffer_append_uint8(&myFbb, 0x65);
  *
  * ...
  *
  * // now retrieve the two values out of the buffer treating them as parts of a 16-bit integer
- * uint16_t foo = cxa_fixedByteBuffer_get_uint16LE(&myBuffer, 0);
- * // foo = 0x6547
+ * uint16_t foo;
+ * if( !cxa_fixedByteBuffer_get_uint16LE(&myFbb, 0, foo) )
+ * {
+ *    // failed to get uint16_t
+ * }
+ * // now, foo = 0x6547
  * @endcode
  *
  *
@@ -85,6 +95,9 @@
 #define cxa_fixedByteBuffer_append_float(fbbIn, floatIn)					cxa_fixedByteBuffer_append((fbbIn), (uint8_t*)&(floatIn), 4)
 #define cxa_fixedByteBuffer_append_cString(fbbIn, strIn)					cxa_fixedByteBuffer_append((fbbIn), (uint8_t*)(strIn), (strlen(strIn)+1))
 
+#define cxa_fixedByteBuffer_append_lengthPrefixedCString_uint16BE(fbbIn, strIn, includeNullTermIn)	\
+	cxa_fixedByteBuffer_append_lengthPrefixedField_uint16BE((fbbIn), (strIn), strlen(strIn) + ((includeNullTermIn) ? 1 : 0))
+
 #define cxa_fixedByteBuffer_remove_uint8(fbbIn, indexIn)					cxa_fixedByteBuffer_remove((fbbIn), (indexIn), 1)
 #define cxa_fixedByteBuffer_remove_uint16(fbbIn, indexIn)					cxa_fixedByteBuffer_remove((fbbIn), (indexIn), 2)
 #define cxa_fixedByteBuffer_remove_uint32(fbbIn, indexIn)					cxa_fixedByteBuffer_remove((fbbIn), (indexIn), 4)
@@ -141,6 +154,7 @@ struct cxa_fixedByteBuffer
  */
 void cxa_fixedByteBuffer_init(cxa_fixedByteBuffer_t *const fbbIn, void *const bufferLocIn, const size_t bufferMaxSize_bytesIn);
 
+
 /**
  * @public
  * @brief Initializes the pre-allocated fixedByteBuffer using the
@@ -155,6 +169,7 @@ void cxa_fixedByteBuffer_init(cxa_fixedByteBuffer_t *const fbbIn, void *const bu
  * @param[in] bufferMaxSize_bytesIn the maximum size of the chunk of memory (buffer) in bytes
  */
 void cxa_fixedByteBuffer_init_inPlace(cxa_fixedByteBuffer_t *const fbbIn, const size_t currNumElemsIn, void *const bufferLocIn, const size_t bufferMaxSize_bytesIn);
+
 
 /**
  * @public
@@ -173,6 +188,7 @@ void cxa_fixedByteBuffer_init_inPlace(cxa_fixedByteBuffer_t *const fbbIn, const 
  */
 void cxa_fixedByteBuffer_init_subBufferFixedSize(cxa_fixedByteBuffer_t *const subFbbIn, cxa_fixedByteBuffer_t *const parentFbbIn, const size_t startIndexIn, size_t numBytesIn);
 
+
 /**
  * @public
  * Initializes a new subBuffer whose bytes are stored within another fixedByteBuffer. This
@@ -188,7 +204,20 @@ void cxa_fixedByteBuffer_init_subBufferFixedSize(cxa_fixedByteBuffer_t *const su
  */
 void cxa_fixedByteBuffer_init_subBufferRemainingElems(cxa_fixedByteBuffer_t *const subFbbIn, cxa_fixedByteBuffer_t *const parentFbbIn, const size_t startIndexIn);
 
+
+/**
+ * @public
+ * Appends (copies) the number of bytes in the specified buffer to the end of the fixed byte buffer
+ *
+ * @param[in] fbbIn pointer to the pre-initialized cxa_fixedByteBuffer_t object
+ * @param[in] ptrIn pointer to the location of the bytes to copy to the end of our buffer
+ * @param[in] numBytesIn the number of bytes, starting at ptrIn to copy to the end of our buffer
+ *
+ * @return true if the append was successful, false if not (eg. full)
+ */
 bool cxa_fixedByteBuffer_append(cxa_fixedByteBuffer_t *const fbbIn, uint8_t *const ptrIn, const size_t numBytesIn);
+bool cxa_fixedByteBuffer_append_lengthPrefixedField_uint16BE(cxa_fixedByteBuffer_t *const fbbIn, uint8_t *const ptrIn, const uint16_t numBytesIn);
+
 bool cxa_fixedByteBuffer_remove(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, const size_t numBytesIn);
 bool cxa_fixedByteBuffer_remove_cString(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn);
 
@@ -196,6 +225,8 @@ uint8_t* cxa_fixedByteBuffer_get_pointerToIndex(cxa_fixedByteBuffer_t *const fbb
 bool cxa_fixedByteBuffer_get(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, bool transposeIn, uint8_t *const valOut, const size_t numBytesIn);
 bool cxa_fixedByteBuffer_get_cString(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, char *const stringOut, size_t maxOutputSize_bytes);
 bool cxa_fixedByteBuffer_get_cString_inPlace(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, char** stringOut, size_t *strLen_bytesOut);
+bool cxa_fixedByteBuffer_get_lengthPrefixedField_uint16BE(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, uint8_t** ptrOut, uint16_t* numBytesOut);
+bool cxa_fixedByteBuffer_get_lengthPrefixedCString_uint16BE(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, char** ptrOut, uint16_t* numBytesOut, bool *isNullTerminatedOut);
 
 bool cxa_fixedByteBuffer_replace(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, uint8_t *const ptrIn, const size_t numBytesIn);
 bool cxa_fixedByteBuffer_replace_cString(cxa_fixedByteBuffer_t *const fbbIn, const size_t indexIn, char *const stringIn);
