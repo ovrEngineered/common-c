017a594b34392d374002c9d3b8d6f5fa3d5f8347
Christopher Armenio <christopher.armenio@opencxa.org>
2015-11-17 22:25:53 -0500

    added initial implementation of a gpio debouncer

---
+++ include/arch-common/cxa_gpio_debouncer.h
@@ -0,0 +1,78 @@
+/**
+ * @file
+ * @copyright 2015 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#ifndef CXA_GPIO_DEBOUNCER_H_
+#define CXA_GPIO_DEBOUNCER_H_
+
+
+// ******** includes ********
+#include <cxa_array.h>
+#include <cxa_gpio.h>
+#include <cxa_timeBase.h>
+#include <cxa_timeDiff.h>
+#include <cxa_config.h>
+
+
+// ******** global macro definitions ********
+#ifndef CXA_GPIO_DEBOUNCER_MAXNUM_LISTENERS
+	#define CXA_GPIO_DEBOUNCER_MAXNUM_LISTENERS			1
+#endif
+
+
+// ******** global type definitions *********
+/**
+ * @public
+ */
+typedef struct cxa_gpio_debouncer cxa_gpio_debouncer_t;
+
+
+/**
+ * @public
+ */
+typedef void (*cxa_gpio_debouncer_cb_onTransition_t)(bool newVal, void *userVarIn);
+
+
+typedef struct
+{
+	cxa_gpio_debouncer_cb_onTransition_t cb_onTransition;
+	void *userVar;
+}cxa_gpio_debouncer_listenerEntry_t;
+
+
+/**
+ * @private
+ */
+struct cxa_gpio_debouncer
+{
+	cxa_timeDiff_t td_debounce;
+	cxa_gpio_t* gpio;
+
+	bool prevVal;
+	bool isInTimeoutPeriod;
+
+	cxa_array_t listeners;
+	cxa_gpio_debouncer_listenerEntry_t listeners_raw[CXA_GPIO_DEBOUNCER_MAXNUM_LISTENERS];
+};
+
+
+// ******** global function prototypes ********
+void cxa_gpio_debouncer_init(cxa_gpio_debouncer_t *const debounceIn, cxa_gpio_t *const gpioIn, cxa_timeBase_t *tbIn);
+
+void cxa_gpio_debouncer_addListener(cxa_gpio_debouncer_t *const debouncerIn, cxa_gpio_debouncer_cb_onTransition_t cb_onTransition, void *userVarIn);
+
+void cxa_gpio_debouncer_update(cxa_gpio_debouncer_t *const debouncerIn);
+
+#endif // CXA_GPIO_DEBOUNCER_H_
---
+++ src/arch-common/cxa_gpio_debouncer.c
@@ -0,0 +1,110 @@
+/**
+ * Copyright 2015 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "cxa_gpio_debouncer.h"
+
+
+/**
+ * @author Christopher Armenio
+ */
+
+
+// ******** includes ********
+#include <cxa_assert.h>
+
+
+// ******** local macro definitions ********
+#define TIMEOUT_PERIOD_MS				100
+
+
+// ******** local type definitions ********
+
+
+// ******** local function prototypes ********
+
+
+// ********  local variable declarations *********
+
+
+// ******** global function implementations ********
+void cxa_gpio_debouncer_init(cxa_gpio_debouncer_t *const debounceIn, cxa_gpio_t *const gpioIn, cxa_timeBase_t *tbIn)
+{
+	cxa_assert(debounceIn);
+	cxa_assert(gpioIn);
+	cxa_assert(cxa_gpio_getDirection(gpioIn) == CXA_GPIO_DIR_INPUT);
+	cxa_assert(tbIn);
+
+	// save our references
+	debounceIn->gpio = gpioIn;
+
+	// set some initial values
+	debounceIn->prevVal = cxa_gpio_getValue(debounceIn->gpio);
+	debounceIn->isInTimeoutPeriod = false;
+
+	// setup our timediff
+	cxa_timeDiff_init(&debounceIn->td_debounce, tbIn, false);
+
+	// setup our listener array
+	cxa_array_initStd(&debounceIn->listeners, debounceIn->listeners_raw);
+}
+
+
+void cxa_gpio_debouncer_addListener(cxa_gpio_debouncer_t *const debouncerIn, cxa_gpio_debouncer_cb_onTransition_t cb_onTransition, void *userVarIn)
+{
+	cxa_assert(debouncerIn);
+
+	cxa_gpio_debouncer_listenerEntry_t newEntry = {
+			.cb_onTransition = cb_onTransition,
+			.userVar = userVarIn
+	};
+	cxa_assert( cxa_array_append(&debouncerIn->listeners, &newEntry) );
+}
+
+
+void cxa_gpio_debouncer_update(cxa_gpio_debouncer_t *const debouncerIn)
+{
+	cxa_assert(debouncerIn);
+
+	if( debouncerIn->isInTimeoutPeriod )
+	{
+		if( cxa_timeDiff_isElapsed_ms(&debouncerIn->td_debounce, TIMEOUT_PERIOD_MS) )
+		{
+			// recheck our value
+			bool currVal = cxa_gpio_getValue(debouncerIn->gpio);
+			if( currVal != debouncerIn->prevVal )
+			{
+				// got a transition...notify our listeners
+				cxa_array_iterate(&debouncerIn->listeners, currListener, cxa_gpio_debouncer_listenerEntry_t)
+				{
+					if( currListener == NULL ) continue;
+					if( currListener->cb_onTransition != NULL ) currListener->cb_onTransition(currVal, currListener->userVar);
+				}
+			}
+
+			// reset
+			debouncerIn->prevVal = currVal;
+			debouncerIn->isInTimeoutPeriod = false;
+		}
+	}
+	else
+	{
+		// not in our timeout...look for new values
+		bool currVal = cxa_gpio_getValue(debouncerIn->gpio);
+		if( currVal != debouncerIn->prevVal ) debouncerIn->isInTimeoutPeriod = true;
+	}
+}
+
+
+// ******** local function implementations ********
