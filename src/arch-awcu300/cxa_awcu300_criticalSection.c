3680478e6b068254714065ed38af0f3b5d59a3ba
Christopher Armenio <christopher.armenio@opencxa.org>
2016-07-11 11:48:11 -0400

    implemented critical section for awcu300

---
+++ src/arch-awcu300/cxa_awcu300_criticalSection.c
@@ -0,0 +1,49 @@
+/**
+ * @copyright 2015 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#include "cxa_criticalSection.h"
+
+
+// ******** includes ********
+#include <FreeRTOS.h>
+#include <task.h>
+
+
+// ******** local macro definitions ********
+
+
+// ******** local type definitions ********
+
+
+// ******** local function prototypes ********
+
+
+// ********  local variable declarations *********
+
+
+// ******** global function implementations ********
+void cxa_criticalSection_enter(void)
+{
+	vTaskSuspendAll();
+}
+
+
+void cxa_criticalSection_exit(void)
+{
+	xTaskResumeAll();
+}
+
+
+// ******** local function implementations ********
