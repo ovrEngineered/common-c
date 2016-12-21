3362fd69ace70d3065ccfdcf5c19b72bad5688e7
Christopher Armenio <christopher.armenio@ovrengineered.com>
2016-12-21 00:04:57 -0500

    Added 128-bite uuid

---
+++ include/misc/cxa_uuid128.h
@@ -0,0 +1,52 @@
+/**
+ * @file
+ * @copyright 2016 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#ifndef CXA_UUID128_H_
+#define CXA_UUID128_H_
+
+
+// ******** includes ********
+#include <stdint.h>
+
+#include <cxa_fixedByteBuffer.h>
+
+
+// ******** global macro definitions ********
+
+
+// ******** global type definitions *********
+typedef struct
+{
+	char str[37];
+}cxa_uuid128_string_t;
+
+
+typedef struct
+{
+	uint8_t bytes[16];
+}cxa_uuid128_t;
+
+
+// ******** global function prototypes ********
+void cxa_uuid128_init(cxa_uuid128_t *const uuidIn, uint8_t *const bytesIn);
+bool cxa_uuid128_initFromBuffer(cxa_uuid128_t *const uuidIn, cxa_fixedByteBuffer_t *const fbbIn, size_t indexIn);
+
+bool cxa_uuid128_isEqual(cxa_uuid128_t *const uuid1In, cxa_uuid128_t *const uuid2In);
+
+void cxa_uuid128_toString(cxa_uuid128_t *const uuidIn, cxa_uuid128_string_t *const strOut);
+void cxa_uuid128_toShortString(cxa_uuid128_t *const uuidIn, cxa_uuid128_string_t *const strOut);
+
+#endif
---
+++ src/misc/cxa_uuid128.c
@@ -0,0 +1,126 @@
+/**
+ * @copyright 2016 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#include "cxa_uuid128.h"
+
+
+// ******** includes ********
+#include <string.h>
+
+#include <cxa_assert.h>
+
+
+// ******** local macro definitions ********
+
+
+// ******** local type definitions ********
+
+
+// ******** local function prototypes ********
+
+
+// ********  local variable declarations *********
+
+
+// ******** global function implementations ********
+void cxa_uuid128_init(cxa_uuid128_t *const uuidIn, uint8_t *const bytesIn)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(bytesIn);
+
+	memcpy(uuidIn->bytes, bytesIn, sizeof(uuidIn->bytes));
+}
+
+
+bool cxa_uuid128_initFromBuffer(cxa_uuid128_t *const uuidIn, cxa_fixedByteBuffer_t *const fbbIn, size_t indexIn)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(fbbIn);
+
+	return cxa_fixedByteBuffer_get(fbbIn, indexIn, false, uuidIn->bytes, sizeof(uuidIn->bytes));
+}
+
+
+bool cxa_uuid128_isEqual(cxa_uuid128_t *const uuid1In, cxa_uuid128_t *const uuid2In)
+{
+	cxa_assert(uuid1In);
+	cxa_assert(uuid2In);
+
+	return (memcmp(uuid1In->bytes, uuid2In->bytes, sizeof(uuid1In->bytes)) == 0);
+}
+
+
+void cxa_uuid128_toString(cxa_uuid128_t *const uuidIn, cxa_uuid128_string_t *const strOut)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(strOut);
+
+	memset(strOut->str, 0, sizeof(strOut));
+
+	char* currStrPtr = strOut->str;
+	uint8_t* currByte = uuidIn->bytes;
+
+	for( int i = 0; i < 4; i++ )
+	{
+		sprintf(currStrPtr, "%02X", *currByte);
+		currStrPtr += 2;
+		currByte++;
+	}
+	*currStrPtr = '-';
+	currStrPtr++;
+	for( int i = 0; i < 2; i++ )
+	{
+		sprintf(currStrPtr, "%02X", *currByte);
+		currStrPtr += 2;
+		currByte++;
+	}
+	*currStrPtr = '-';
+	currStrPtr++;
+	for( int i = 0; i < 2; i++ )
+	{
+		sprintf(currStrPtr, "%02X", *currByte);
+		currStrPtr += 2;
+		currByte++;
+	}
+	*currStrPtr = '-';
+	currStrPtr++;
+	for( int i = 0; i < 2; i++ )
+	{
+		sprintf(currStrPtr, "%02X", *currByte);
+		currStrPtr += 2;
+		currByte++;
+	}
+	*currStrPtr = '-';
+	currStrPtr++;
+	for( int i = 0; i < 6; i++ )
+	{
+		sprintf(currStrPtr, "%02X", *currByte);
+		currStrPtr += 2;
+		currByte++;
+	}
+}
+
+
+void cxa_uuid128_toShortString(cxa_uuid128_t *const uuidIn, cxa_uuid128_string_t *const strOut)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(strOut);
+
+	cxa_uuid128_toString(uuidIn, strOut);
+	strOut->str[7] = 0;
+}
+
+
+// ******** local function implementations ********
