d9e9127b9c27652e80931ef8daf1c9e85fd4ed76
Christopher Armenio <christopher.armenio@ovrengineered.com>
2017-01-15 20:22:06 -0500

    Added eui48 implementation

---
+++ include/misc/cxa_eui48.h
@@ -0,0 +1,53 @@
+/**
+ * @file
+ * @copyright 2016 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#ifndef CXA_eui48_H_
+#define CXA_eui48_H_
+
+
+// ******** includes ********
+#include <stdint.h>
+
+#include <cxa_fixedByteBuffer.h>
+
+
+// ******** global macro definitions ********
+
+
+// ******** global type definitions *********
+typedef struct
+{
+	char str[18];
+}cxa_eui48_string_t;
+
+
+typedef struct
+{
+	uint8_t bytes[6];
+}cxa_eui48_t;
+
+
+// ******** global function prototypes ********
+void cxa_eui48_init(cxa_eui48_t *const uuidIn, uint8_t *const bytesIn);
+bool cxa_eui48_initFromBuffer(cxa_eui48_t *const uuidIn, cxa_fixedByteBuffer_t *const fbbIn, size_t indexIn);
+void cxa_eui48_initRandom(cxa_eui48_t *const uuidIn);
+
+bool cxa_eui48_isEqual(cxa_eui48_t *const uuid1In, cxa_eui48_t *const uuid2In);
+
+void cxa_eui48_toString(cxa_eui48_t *const uuidIn, cxa_eui48_string_t *const strOut);
+void cxa_eui48_toShortString(cxa_eui48_t *const uuidIn, cxa_eui48_string_t *const strOut);
+
+#endif
---
+++ src/misc/cxa_eui48.c
@@ -0,0 +1,119 @@
+/**
+ * @copyright 2016 opencxa.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Christopher Armenio
+ */
+#include "cxa_eui48.h"
+
+
+// ******** includes ********
+#include <stdlib.h>
+#include <string.h>
+
+#include <cxa_assert.h>
+#include <cxa_timeBase.h>
+
+#define CXA_LOG_LEVEL				CXA_LOG_LEVEL_TRACE
+#include <cxa_logger_implementation.h>
+
+// ******** local macro definitions ********
+
+
+// ******** local type definitions ********
+
+
+// ******** local function prototypes ********
+
+
+// ********  local variable declarations *********
+
+
+// ******** global function implementations ********
+void cxa_eui48_init(cxa_eui48_t *const uuidIn, uint8_t *const bytesIn)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(bytesIn);
+
+	memcpy(uuidIn->bytes, bytesIn, sizeof(uuidIn->bytes));
+}
+
+
+bool cxa_eui48_initFromBuffer(cxa_eui48_t *const uuidIn, cxa_fixedByteBuffer_t *const fbbIn, size_t indexIn)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(fbbIn);
+
+	return cxa_fixedByteBuffer_get(fbbIn, indexIn, false, uuidIn->bytes, sizeof(uuidIn->bytes));
+}
+
+
+void cxa_eui48_initRandom(cxa_eui48_t *const uuidIn)
+{
+	cxa_assert(uuidIn);
+
+	srand(cxa_timeBase_getCount_us());
+	for( size_t i = 0; i < sizeof(uuidIn->bytes); i++ )
+	{
+		// generates [0-255]
+		uuidIn->bytes[i] = rand() % (255 + 1 - 0) + 0;
+	}
+}
+
+
+bool cxa_eui48_isEqual(cxa_eui48_t *const uuid1In, cxa_eui48_t *const uuid2In)
+{
+	cxa_assert(uuid1In);
+	cxa_assert(uuid2In);
+
+	return (memcmp(uuid1In->bytes, uuid2In->bytes, sizeof(uuid1In->bytes)) == 0);
+}
+
+
+void cxa_eui48_toString(cxa_eui48_t *const uuidIn, cxa_eui48_string_t *const strOut)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(strOut);
+
+	memset(strOut->str, 0, sizeof(strOut->str));
+	char* currStrPtr = strOut->str;
+
+	size_t numBytes = sizeof(uuidIn->bytes);
+	for( int i = 0; i < numBytes; i++ )
+	{
+		if( i != (numBytes - 1) )
+		{
+			sprintf(currStrPtr, "%02X:", uuidIn->bytes[numBytes-i-1]);
+			currStrPtr += 3;
+		}
+		else
+		{
+			sprintf(currStrPtr, "%02X", uuidIn->bytes[numBytes-i-1]);
+			currStrPtr += 2;
+		}
+	}
+}
+
+
+void cxa_eui48_toShortString(cxa_eui48_t *const uuidIn, cxa_eui48_string_t *const strOut)
+{
+	cxa_assert(uuidIn);
+	cxa_assert(strOut);
+
+	cxa_eui48_string_t tmpStr;
+	cxa_eui48_toString(uuidIn, &tmpStr);
+	strcpy(strOut->str, tmpStr.str+sizeof(tmpStr.str)-6);
+	strOut->str[5] = 0;
+}
+
+
+// ******** local function implementations ********
