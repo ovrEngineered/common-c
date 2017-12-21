ccccb6ff39327bc3f8a9dbc84600157a89239b71
Christopher Armenio <christopher.armenio@ovrengineered.com>
2017-12-21 12:12:20 -0500

    updated esp32 uniqueID to contain colons to better separate characters

--- src/arch-esp32/cxa_esp32_uniqueId.c
+++ src/arch-esp32/cxa_esp32_uniqueId.c
@@ -36,7 +36,7 @@ static void init(void);
 // ********  local variable declarations *********
 static bool isInit = false;
 static uint8_t id_bytes[6];
-static char id_str[13];
+static char id_str[18];
 
 
 // ******** global function implementations ********
@@ -62,7 +62,7 @@ static void init(void)
 {
 	esp_wifi_get_mac(WIFI_IF_STA, id_bytes);
 
-	sprintf(id_str, "%02X%02X%02X%02X%02X%02X", id_bytes[0], id_bytes[1], id_bytes[2], id_bytes[3], id_bytes[4], id_bytes[5]);
+	sprintf(id_str, "%02X:%02X:%02X:%02X:%02X:%02X", id_bytes[0], id_bytes[1], id_bytes[2], id_bytes[3], id_bytes[4], id_bytes[5]);
 	id_str[sizeof(id_str)-1] = 0;
 
 	isInit = true;
