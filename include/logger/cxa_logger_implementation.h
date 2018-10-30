206a27dd782a9d28338ae12570724812ac0aaf78
Christopher Armenio <christopher.armenio@breakwall.io>
2018-10-30 09:37:50 -0400

    COMMON - fixed bug with logger step debugs

--- include/logger/cxa_logger_implementation.h
+++ include/logger/cxa_logger_implementation.h
@@ -121,8 +121,8 @@
 	#error "Unknown CXA_LOG_LEVEL specified"
 #endif
 
-#define cxa_logger_stepDebug()								cxa_logger_stepDebug_formattedString(__FILE__, __LINE__, NULL)
-#define cxa_logger_stepDebug_msg(msgIn, ...)				cxa_logger_stepDebug_formattedString(__FILE__, __LINE__, (msgIn), ##__VA_ARGS__)
+#define cxa_logger_stepDebug()								cxa_logger_stepDebug_formattedString_impl(__FILE__, __LINE__, NULL)
+#define cxa_logger_stepDebug_msg(msgIn, ...)				cxa_logger_stepDebug_formattedString_impl(__FILE__, __LINE__, (msgIn), ##__VA_ARGS__)
 #define cxa_logger_stepDebug_memDump(msgIn, ptrIn, lenIn)	cxa_logger_stepDebug_memDump_impl(__FILE__, __LINE__, (ptrIn), (lenIn), (msgIn))
 #define cxa_logger_stepDebug_memDump_fbb(msgIn, fbbIn)		cxa_logger_stepDebug_memDump_impl(__FILE__, __LINE__, cxa_fixedByteBuffer_get_pointerToIndex((fbbIn),0), cxa_fixedByteBuffer_getSize_bytes((fbbIn)), (msgIn))
 
