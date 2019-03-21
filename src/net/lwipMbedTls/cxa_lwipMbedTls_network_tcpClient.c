dfc1c8887867f5be7504ec975fa200d775aea458
Christopher Armenio <christopher.armenio@breakwall.io>
2019-03-20 21:02:08 -0400

    ESP32 - updated tcpClient to work without client certificates

--- src/net/lwipMbedTls/cxa_lwipMbedTls_network_tcpClient.c
+++ src/net/lwipMbedTls/cxa_lwipMbedTls_network_tcpClient.c
@@ -55,6 +55,8 @@ typedef enum
 // ******** local function prototypes ********
 static void cleanupConnectionResources(cxa_lwipMbedTls_network_tcpClient_t *const netClientIn);
 
+static bool scm_connectToHost(cxa_network_tcpClient_t *const superIn, char *const hostNameIn, uint16_t portNumIn, bool useTlsIn, uint32_t timeout_msIn);
+
 static bool scm_connectToHost_clientCert(cxa_network_tcpClient_t *const superIn, char *const hostNameIn, uint16_t portNumIn,
 																	 const char* serverRootCertIn, size_t serverRootCertLen_bytesIn,
 																	 const char* clientCertIn, size_t clientCertLen_bytesIn,
@@ -99,7 +101,7 @@ void cxa_lwipMbedTls_network_tcpClient_init(cxa_lwipMbedTls_network_tcpClient_t 
 	cxa_stateMachine_setInitialState(&netClientIn->stateMachine, STATE_IDLE);
 
 	// initialize our super class
-	cxa_network_tcpClient_init(&netClientIn->super, NULL, scm_connectToHost_clientCert, scm_disconnectFromHost, scm_isConnected);
+	cxa_network_tcpClient_init(&netClientIn->super, scm_connectToHost, scm_connectToHost_clientCert, scm_disconnectFromHost, scm_isConnected);
 }
 
 
@@ -117,6 +119,93 @@ static void cleanupConnectionResources(cxa_lwipMbedTls_network_tcpClient_t *cons
 
 	// Free referenced items in an SSL context and clear memory
 	mbedtls_ssl_free(&netClientIn->tls.sslContext);
+}
+
+
+static bool scm_connectToHost(cxa_network_tcpClient_t *const superIn, char *const hostNameIn, uint16_t portNumIn, bool useTlsIn, uint32_t timeout_msIn)
+{
+	cxa_assert(hostNameIn);
+	cxa_assert(useTlsIn);
+
+	cxa_lwipMbedTls_network_tcpClient_t* netClientIn = (cxa_lwipMbedTls_network_tcpClient_t*)superIn;
+	cxa_assert(netClientIn);
+
+	// make sure we are currently idle
+	if( cxa_stateMachine_getCurrentState(&netClientIn->stateMachine) != STATE_IDLE )
+	{
+		cxa_logger_trace(&netClientIn->super.logger, "not idle, cannot connect");
+		return false;
+	}
+
+	int tmpRet;
+
+	// initialize our basics if needed
+	if( !netClientIn->tls.initState.areBasicsInitialized )
+	{
+		cxa_logger_trace(&netClientIn->super.logger, "initializing TLS basics");
+
+		// basic initialization
+		mbedtls_ssl_init(&netClientIn->tls.sslContext);
+		mbedtls_ctr_drbg_init(&netClientIn->tls.ctr_drbg);
+		mbedtls_ssl_config_init(&netClientIn->tls.conf);
+		mbedtls_entropy_init(&netClientIn->tls.entropy);
+
+
+		char* personalizationString = cxa_uniqueId_getHexString();
+		tmpRet = mbedtls_ctr_drbg_seed(&netClientIn->tls.ctr_drbg, mbedtls_entropy_func, &netClientIn->tls.entropy,
+										   (const unsigned char *)personalizationString, strlen(personalizationString));
+		if( tmpRet != 0 )
+		{
+			cxa_logger_warn(&netClientIn->super.logger, "failed to seed tls drbg: %d", tmpRet);
+			return false;
+		}
+		netClientIn->tls.initState.areBasicsInitialized = true;
+	}
+
+	// server hostname
+	if( !cxa_stringUtils_equals_ignoreCase(hostNameIn, netClientIn->targetHostName) )
+	{
+		cxa_logger_trace(&netClientIn->super.logger, "setting tls hostname");
+		tmpRet = mbedtls_ssl_set_hostname(&netClientIn->tls.sslContext, hostNameIn);
+	    if( tmpRet < 0 )
+	    {
+	    	cxa_logger_warn(&netClientIn->super.logger, "failed to set tls hostname: %s0x%x", tmpRet<0?"-":"", tmpRet<0?-(unsigned)tmpRet:tmpRet);
+			return false;
+	    }
+	    strlcpy(netClientIn->targetHostName, hostNameIn, sizeof(netClientIn->targetHostName));
+	}
+
+	// SSL configuration
+	cxa_logger_trace(&netClientIn->super.logger, "configuring tls context");
+	tmpRet = mbedtls_ssl_config_defaults(&netClientIn->tls.conf, MBEDTLS_SSL_IS_CLIENT, MBEDTLS_SSL_TRANSPORT_STREAM, MBEDTLS_SSL_PRESET_DEFAULT);
+	if( tmpRet < 0 )
+	{
+		cxa_logger_warn(&netClientIn->super.logger, "tls configuration failed: %s0x%x", tmpRet<0?"-":"", tmpRet<0?-(unsigned)tmpRet:tmpRet);
+		return false;
+	}
+    mbedtls_ssl_conf_authmode(&netClientIn->tls.conf, MBEDTLS_SSL_VERIFY_NONE);
+	mbedtls_ssl_conf_rng(&netClientIn->tls.conf, mbedtls_ctr_drbg_random, &netClientIn->tls.ctr_drbg);
+
+#ifdef MBEDTLS_DEBUG_C
+    mbedtls_esp_enable_debug_log(&netClientIn->tls.conf, DEBUG_LEVEL);
+#endif
+
+	tmpRet = mbedtls_ssl_setup(&netClientIn->tls.sslContext, &netClientIn->tls.conf);
+	if( tmpRet < 0 )
+	{
+		cxa_logger_warn(&netClientIn->super.logger, "tls context configuration failed: %s0x%x", tmpRet<0?"-":"", tmpRet<0?-(unsigned)tmpRet:tmpRet);
+		return false;
+	}
+
+	// make sure we record other useful information
+	netClientIn->useClientCert = false;
+	snprintf(netClientIn->targetPortNum, sizeof(netClientIn->targetPortNum), "%d", portNumIn);
+	netClientIn->targetPortNum[sizeof(netClientIn->targetPortNum)-1] = 0;
+
+	// start the connection
+	cxa_stateMachine_transition(&netClientIn->stateMachine, STATE_CONNECTING);
+
+	return true;
 }
 
 
@@ -322,7 +411,34 @@ static void stateCb_connecting_state(cxa_stateMachine_t *const smIn, void *userV
 //			return;
 //		}
 	}
-	else cxa_assert(false);
+	else
+	{
+		int tmpRet;
+		mbedtls_net_init(&netClientIn->tls.server_fd);
+
+		cxa_logger_info(&netClientIn->super.logger, "connecting to '%s:%s", netClientIn->targetHostName, netClientIn->targetPortNum);
+		tmpRet = mbedtls_net_connect(&netClientIn->tls.server_fd, netClientIn->targetHostName, netClientIn->targetPortNum, MBEDTLS_NET_PROTO_TCP);
+		if( tmpRet < 0 )
+		{
+			cxa_logger_warn(&netClientIn->super.logger, "connect failed: %d", tmpRet);
+			cxa_stateMachine_transition(&netClientIn->stateMachine, STATE_CONNECT_FAIL);
+			return;
+		}
+
+		// set the byte IO callbacks for our context/descriptor
+		mbedtls_ssl_set_bio(&netClientIn->tls.sslContext, &netClientIn->tls.server_fd, mbedtls_net_send, mbedtls_net_recv, NULL);
+
+		cxa_logger_trace(&netClientIn->super.logger, "performing TLS handshake");
+		while( (tmpRet = mbedtls_ssl_handshake(&netClientIn->tls.sslContext)) != 0 )
+		{
+			if( (tmpRet != MBEDTLS_ERR_SSL_WANT_READ) && (tmpRet != MBEDTLS_ERR_SSL_WANT_WRITE) )
+			{
+				cxa_logger_warn(&netClientIn->super.logger, "TLS handshake failed: %s0x%x", tmpRet<0?"-":"", tmpRet<0?-(unsigned)tmpRet:tmpRet);
+				cxa_stateMachine_transition(&netClientIn->stateMachine, STATE_CONNECT_FAIL);
+				return;
+			}
+		}
+	}
 
 	cxa_stateMachine_transition(&netClientIn->stateMachine, STATE_CONNECTED);
 }
